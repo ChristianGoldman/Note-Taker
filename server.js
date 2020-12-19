d801c02d4c78f2b288b08b8f066500e75ef002a9
Christian Goldman <christian.goldman9638@gmail.com>
2020-12-18 23:04:26 -0600

    api post and delete routes logic finished

--- server.js
+++ server.js
@@ -2,54 +2,86 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const notes = require("./db/db.json");
-
+// setting up express app
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-
+// sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 
 console.log((path.join(__dirname, "/public/index.html")));
-
+// path to home page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-// path to notes page
 
+// path to notes page
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
-
+// path to 
 app.get("/api/notes", function (req, res) {
     res.json(notes);
 })
-// api routes for getting the notes from db.json
-// json.parse on db.json
-// res.json(path.join(__dirname, "/db/db.json"))
-// 3 api routes 
-// app.get
-// app.post
-// app.delete/destroy
 app.post("/api/notes", function (req, res) {
-    req.body
-    // get all data from db.json
-    // add id possibly loop to make sure no duplicate id's
-    // save data to variable
-    // updatge db.json with new notes
-    // send new data back to the front end
-    // check gitlab notes
+    // setting user input objest to variable
+    let newNote = req.body;
+    // creating variable to be used later
+    let idUpdate;
+    // setting up if statement if user wishes to update notes
+    if(req.body.id){
+        // setting variable equal to object id
+        idUpdate = req.body.id;
+    } else {
+        // if no update set id to start at 1
+        req.body.id = notes.length + 1;
+    }
+    let existingNote;
+    // looping through notes object to increment id by 1
+    for (let i = 0; i < notes.length; i++) {
+        let note = notes[i];
+        if(note.id === parseInt(idUpdate)) {
+            existingNote = note;
+        }
+    }
+    // setting existing note values equal to note values
+    if(existingNote) {
+        existingNote.title = newNote.title;
+        existingNote.text = newNote.text;
+    } else {
+    }
+    // pushing newNote to db.json array
+    notes.push(newNote);
+    // writing the file and turning the JSON object into a string
+    fs.writeFile("db/db.json", JSON.stringify(notes), "utf8", function(err){
+        if (err) throw err;
+        console.log("Note saved!");
+    });
+    // returning updated note object to front end
+    res.send(notes);
 })
 
 app.delete("/api/notes/:id", function (req, res) {
-    // read from db.json
-    // identify specific id's
-    // get rid of that specific id
-    // re save the file with the new note gone/ update data base with deleted note
-    // res.send new set of notes
+    // setting variable equal to object id
+    const idDelete = req.params.id;
+    // looping through object to determine what note user wishes to delete
+    for (let i = 0; i < notes.length; i++) {
+        let note = notes[i];
+        if(note.id === parseInt(idDelete)) {
+            // using splice to delete note and index of I and deleting one element
+            notes.splice(i, 1);
+        }
+    }
+// writingt file to db.json and turning object to string
+    fs.writeFile("db/db.json", JSON.stringify(notes), "utf8", function(err){
+        if (err) throw err;
+    });
+// returning updated deleted note to the front end
+    res.send(notes);
 })
-
+// starts the server to being listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
